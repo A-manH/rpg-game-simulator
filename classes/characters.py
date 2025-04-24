58d79acea5ac0471f88ecf9966c2f387fc6dff0c
A-manH <amanhuss3n@gmail.com>
2025-04-24 12:48:55 -0400

    set up file structure

---
+++ classes/characters.py
@@ -0,0 +1,24 @@
+class Character:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.moveset = []
+
+    def attack():
+        pass
+
+    def defend():
+        pass
+
+class Warrior(Character):
+    pass
+
+class Brute(Character):
+    pass
+
+class Ninja(Character):
+    pass
+
+class Healer(Character):
+    pass
+
